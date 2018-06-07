cdf824aea782279591bfcc81f82687a6ce3df131
Jon Gabilondo <jon.gabilondo@hp.com>
2018-06-07 15:52:58 +0300

    Removed unused third party code.

--- js-third-party/flux/scripts/jest/preprocessor.js
+++
@@ -1,9 +0,0 @@
-var assign = require('object-assign');
-var babel = require('babel-core');
-var babelOpts = require('../babel/default-options');
-
-module.exports = {
-  process: function(src, path) {
-    return babel.transform(src, assign({filename: path}, babelOpts)).code;
-  }
-};
